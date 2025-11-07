8c2c0848ee94423a7a6bb2ae5ef0191062b23369
yehuda-d <yehuda130@gmail.com>
2025-11-07 09:05:36 +0200

    added env

--- lesson1 4.11.25/index.js
+++ lesson1 4.11.25/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
-const port = 3800;
+const port = process.env.PORT;
+const api = process.env.HOST
 
-app.listen(port, () => {console.log(`http://localhost:${port}`);});
+app.listen(port, () => {console.log(`http://${api}:${port}`);});
