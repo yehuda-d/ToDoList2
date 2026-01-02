f695081f33a0ebf8db803b37c81f194d1bb804f6
yehuda-d <yehuda130@gmail.com>
2026-01-02 11:13:13 +0200

    we now can see the category name in table

--- lesson1 4.11.25/public/scripts/script.js
+++ lesson1 4.11.25/public/scripts/script.js
@@ -1,6 +1,7 @@
 let greating = " hello ";
 greating += localStorage.getItem('name');
  document.getElementById("greating").innerHTML = greating;
+ allCategories = [];
 
 
 async function getTasks() {
@@ -29,10 +30,14 @@ function creatTable(data) {
               if(obj){
                   let check = obj.isDone ? "checked" : "";
                   let rowClass = obj.isDone ? "class =rowClass" : "";
+                  console.log(obj.category_ID);
+                  console.log(allCategories);
+                  
+                  let categoryName = allCategories[obj.category_ID] ? allCategories[obj.category_ID].categoryName : '--';
                   txt += `<tr ${rowClass}>`;
                   txt += `<td><input type="checkbox" ${check} onchange = "taskDone(${obj.id},this)"></td>`
                   txt += `<td>${obj.text}</td>`;
-                  txt += `<td>${obj.category_ID}</td>`;
+                  txt += `<td>${categoryName}</td>`;
                 //   txt += `<td><button onclick = "toggleDone(${obj.id}, ${obj.isDone})">${obj.isDone ? '☑️' : '☐'}</button></td>`;
                   txt += `<td><button onclick = "taskById(${obj.id})">✏️</button></td>`;
                   txt += `<td><button onclick = "deleteTask(${obj.id})">🗑️</button></td>`;
@@ -57,4 +62,30 @@ function creatTable(data) {
                 alert(err);
             }
         }
+        async function getCategories() {
+    try {
+        let response = await fetch('/categories')
+        if(response.status == 401){
+            window.location.href = '/login';
+            return;
+         }
+         let data = await response.json();
+        if(response.status == 400){
+            alert(data.message);
+            return;
+        }
+        for(let c of data){
+            allCategories[c.id] = c;
+        }
+        }
+     catch (err) {
+        alert(err);
+    }
+}
+
+function selectCategories(){
+    
+}
+
+getCategories();
 getTasks();
